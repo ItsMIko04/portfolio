27e466fe0cdc773f7059df758354d4abd02fba8b
ItsMIko04 <pesqq.mico@gmail.com>
2026-05-18 15:56:31 +0200

    hard-skills e progetti

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,10 @@ import { Routes } from '@angular/router';
 import { Experience } from './components/experience/experience';
 import { Contact } from './components/contact/contact';
 import { Informazioni } from './components/informazioni/informazioni';
+import { Home } from './components/home/home';
 
 export const routes: Routes = [
-    { path: '', children: [] },
+    { path: '', component: Home }, 
     { path: 'informazioni', component: Informazioni },
     { path: 'esperienza', component: Experience },
     { path: 'contattami', component: Contact },
---
+++ src/app/components/home/home.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { Hero } from '../hero/hero';
+import { Skills } from '../skills/skills';
+
+@Component({
+  selector: 'app-home',
+  imports: [Hero, Skills],
+  templateUrl: './home.html',
+  styleUrl: './home.css',
+})
+export class Home {}
